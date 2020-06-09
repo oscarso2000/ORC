eeee9f2344ec30205fb745503b720ae5e4403663
Oscar <os2304@columbia.edu>
2020-06-09 11:38:24 +0800

    Initial Commit

---
+++ ORC/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  ORC
+//
+//  Created by Oscar So on 6/9/20.
+//  Copyright © 2020 Oscar So. All rights reserved.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
